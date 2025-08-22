b4a45f0e25b5d5b432d998c00917f5490a5f9123
Roshan <roshanadithya965@gmail.com>
2025-08-22 22:44:28 +0530

    Waiting for Api

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,15 @@
 import {useState} from "react"
 import ApiContent from "./ApiContent"
+import Recipe from "./Recipe"
 export default function MainContent(){
     const [foodItems,setFoodItems] = useState([])
-    
+    const [showRecipe,setShowRecipe] = useState(false)
     function submit(FormData){
         const Ingredient = FormData.get("Ingredient")
         setFoodItems((prevFoodItems)=>[...prevFoodItems,Ingredient])
+    }
+    function toggleRecipe(){
+        setShowRecipe((prevShowRecipe)=>!prevShowRecipe)
     }
     return(
         <main>
@@ -14,8 +18,11 @@ export default function MainContent(){
         <button>+ Add Ingredient</button>
         </form>
         {foodItems.length > 0 ? <section >
-        <ApiContent foodItems = {foodItems}/>
+        <ApiContent foodItems = {foodItems} toggleRecipe={toggleRecipe}/>
         </section>:null}
+        <section>
+           { showRecipe ? <Recipe/> : null}
+        </section>
         </main>
     )
 }
